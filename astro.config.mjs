602a0e93cd94a8f7818fac095c90da067609862f
Not TCA <50563138+ToastedDev@users.noreply.github.com>
2023-12-20 16:11:05 +0800

    feat(config): add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://toasted.dev",
+  integrations: [tailwind(), sitemap()],
 });
