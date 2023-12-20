9a2377bebd9de01d676666b0705faa43731262bc
Not TCA <50563138+ToastedDev@users.noreply.github.com>
2023-12-20 15:58:58 +0800

    fix(config): remove vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,7 @@
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "static",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
 });
