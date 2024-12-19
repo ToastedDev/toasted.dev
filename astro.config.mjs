8a78823ae1139a8c6a9194dd8202925198409b01
ToastedToast <hey@toasted.dev>
2024-12-19 13:43:16 +0800

    Revert "feat(homepage): add current time section"
    This reverts commit 76e1ce1e06dca096b5878b8a5ea94bda023b53fe.

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
 function remarkReadingTime() {
-  return function (tree, { data }) {
+  return function(tree, { data }) {
     const textOnPage = toString(tree);
     const readingTime = getReadingTime(textOnPage);
     // readingTime.text will give us minutes read as a friendly string,
